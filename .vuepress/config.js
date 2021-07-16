7bb887074399ded899283780b836352fa697d2b8
1204714409@qq.com <1204714409@qq.com>
2021-07-16 18:09:22 +0800

    初次提交

---
+++ .vuepress/config.js
@@ -0,0 +1,29 @@
+// .vuepress/config.js
+module.exports = {
+  themeConfig: {
+    sidebar: {
+      '/foo/': [
+        '',     /* /foo/ */
+        'one',  /* /foo/one.html */
+        'two'   /* /foo/two.html */
+      ],
+
+      '/bar/': [
+        '',      /* /bar/ */
+        'three', /* /bar/three.html */
+        'four'   /* /bar/four.html */
+      ],
+
+      // fallback
+      '/': [
+        '',        /* / */
+        'contact', /* /contact.html */
+        'about'    /* /about.html */
+      ]
+    }
+  }
+}
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around'
+  }
